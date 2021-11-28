beb19954f84ee58e4fdd801f0ba2eb3178185aaa
Aditya Paranjape <87206667+FlamingDragon12@users.noreply.github.com>
2021-11-28 17:56:05 +0530

    initial commit

---
+++ scrip.js
@@ -0,0 +1,23 @@
+// alert("shit")
+setInterval(setClock,1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minHand = document.querySelector('[data-min-hand]')
+const secondHand = document.querySelector('[data-sec-hand]')
+
+function setClock() {
+	const currentDate = new Date()
+	const secondsRatio = currentDate.getSeconds() / 60
+	const minutesRatio = ( secondsRatio+ currentDate.getMinutes()) / 60
+	const hoursRatio = ( minutesRatio+ currentDate.getHours()) / 12
+    setRotation(secondHand, secondsRatio)
+    setRotation(minHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+
+}
+
+function setRotation(element,rotationRatio) {
+	element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
